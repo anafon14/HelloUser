32d7e0e2fcddd2e7d05d7b6ac8402a92886aaa88
anafon14 <a.fonsecateixei@sjasd.ca>
2016-10-20 09:52:52 -0500

    Assignment 2 Project 1
    Hello User assignment to Mr. Hardman

---
+++ HelloUser/frmHelloUser.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace HelloUser
+{
+    partial class frmHelloUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.lblDone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(80, 12);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(89, 39);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "Click me first!";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Location = new System.Drawing.Point(12, 54);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(0, 13);
+            this.lblDisplay.TabIndex = 1;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(14, 87);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(58, 13);
+            this.lblFirstName.TabIndex = 2;
+            this.lblFirstName.Text = "First name:";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(13, 111);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(59, 13);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Last name:";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(13, 134);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 13);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Age:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(95, 84);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 5;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.txtFirstName_TextChanged);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(95, 108);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 6;
+            this.txtLastName.TextChanged += new System.EventHandler(this.txtLastName_TextChanged);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(95, 131);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 7;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(80, 154);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(75, 23);
+            this.btnDone.TabIndex = 8;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // lblDone
+            // 
+            this.lblDone.AutoSize = true;
+            this.lblDone.Location = new System.Drawing.Point(23, 192);
+            this.lblDone.Name = "lblDone";
+            this.lblDone.Size = new System.Drawing.Size(0, 13);
+            this.lblDone.TabIndex = 9;
+            // 
+            // frmHelloUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblDone);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.btnClickMe);
+            this.Name = "frmHelloUser";
+            this.Text = "HelloUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Label lblDone;
+    }
+}
+
---
+++ HelloUser/frmHelloUser.cs
@@ -0,0 +1,110 @@
+﻿/**
+
+ * Name: Ana Luiza Fonseca Teixeira
+
+ * Teacher: Mr. Hadrman
+
+ * Assignment #2, Program #1
+
+ * Date Last Modified: 10/17/2016
+
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloUser
+{
+    public partial class frmHelloUser : Form
+    {
+        public frmHelloUser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "Hello User! Can you please fill out the information below?";
+        }
+
+        private void txtFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLastName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+
+            //variables are created 
+            String userName;
+            String userLastName;
+            double userAge;
+            double ageDifference;
+            double secondAgeDifference;
+
+          
+            userName = txtFirstName.Text;
+            userLastName = txtLastName.Text;
+            userAge = Convert.ToDouble(txtAge.Text);
+            ageDifference = 14 - userAge;
+            secondAgeDifference = userAge - 14;
+
+            // display this if user is younger than 13 years old
+            if (userAge < 13)
+            {
+
+                lblDone.Text = " Nice to meet you " + userName + " " + userLastName + '!'.ToString() + "\n You are " + ageDifference + " years younger than me!";
+
+            }
+
+            //display this if user is 13 years old
+            else if (userAge == 13)
+            {
+
+                lblDone.Text = " Nice to meet you " + userName + " " + userLastName + '!'.ToString() + "\n You are one year younger than me!";
+
+            }
+
+            //display this if user is 14 years old
+            else if (userAge == 14)
+            {
+
+                lblDone.Text = " Nice to meet you " + userName + " " + userLastName + '!'.ToString() + "\n You are the same age as me!";
+            }
+
+            //display this if user is 15 years old
+            else if (userAge == 15)
+            {
+
+                lblDone.Text = " Nice to meet you " + userName + " " + userLastName + '!'.ToString() + "\n You are one year older than me!";
+
+            }
+
+            //display this if user's age doesn't fit any of the descriptions above, meaning the user is older than 15
+            else
+            {
+
+                lblDone.Text = " Nice to meet you " + userName + " " + userLastName + '!'.ToString() + "\n You are " + secondAgeDifference + " years older than me!";
+            }
+
+        }
+    }
+}
